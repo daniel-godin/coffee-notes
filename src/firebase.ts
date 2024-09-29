d820e79471454ad39fb9f678ce44dba03ae28e8e
Daniel Godin <DanielEGodin@gmail.com>
2024-09-29 11:17:33 -0700

    Boilerplate removal & Firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { firebaseConfig } from "../firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+// Firebase Emulators:
+// Comment these out when you're ready to deploy to prod, or when you want to access prod.
+if (location.hostname === 'localhost' || '127.0.0.1') {
+    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+    connectAuthEmulator(auth, "http://127.0.0.1:9099");
+    connectStorageEmulator(storage, "127.0.0.1", 9199);
+}

0df6051d4f5a023471a554b4d55307cb323ec19d
Daniel Godin <DanielEGodin@gmail.com>
2024-10-01 07:38:11 -0700

    id is now added when a coffee note is created.  Also grabbing all coffee notes from a user collection displays in coffee notes page.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createNav } from './nav';
 import { setupAuth } from './auth';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { auth, db } from './firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, doc, getDocs, setDoc } from 'firebase/firestore';
 
 
 initApp();
@@ -37,20 +37,14 @@ function createMainContent(user:User, container: HTMLElement) {
     // container.innerHTML = '';
     // container.insertAdjacentHTML('afterbegin', mainContent);
 
-    displayCreateCoffeeNote(user, container);
+    // displayCreateCoffeeNote(user, container);
+    displayCoffeeNotes(user, container);
 }
 
 function displayCreateCoffeeNote(user:User, container: HTMLElement) {
     const coffeeNotePageContainer = `<div id='createCoffeeNotePageContainer'><div>`
     container.innerHTML = '';
     container.insertAdjacentHTML('afterbegin', coffeeNotePageContainer);
-
-    const formElements = {
-        'dateCreated': new Date(),
-        'dateTasted': Date,
-
-
-    }
 
     const formCreateCoffeeNoteBasic = `
     <form id='createCoffeeNoteForm' class='form-default form-coffee-notes'>
@@ -82,7 +76,14 @@ function displayCreateCoffeeNote(user:User, container: HTMLElement) {
         console.log('coffee note creation submitted');
         const formData = new FormData(e.target);
 
+        // Get a reference to the user's 'coffee-notes' collection
+        const coffeeNotesRef = collection(db, 'users', user.uid, 'coffee-notes');
+
+        // Generate a new document reference (this doesn't write to the database)
+        const newDocRef = doc(coffeeNotesRef);
+
         const coffeeNoteData = {
+            'id': newDocRef.id, // Include the ID in the initial data
             'coffee': formData.get('coffee'),
             'dateTried': formData.get('dateTried') || '',
             'rating': formData.get('rating') ? parseFloat(formData.get('rating')) : null,
@@ -90,13 +91,48 @@ function displayCreateCoffeeNote(user:User, container: HTMLElement) {
         }
 
         try {
-            await addDoc(collection(db, 'users', user.uid, 'coffee-notes'), coffeeNoteData);
+            await setDoc(newDocRef, coffeeNoteData); // Using setDoc, instead of addDoc, because I've already created the document id above.
         } catch (error) {
             console.error("Error adding coffee note to document database:", error);
         }
     })
 }
 
-function displayCoffeeNotes() {
+async function displayCoffeeNotes(user: User, container: HTMLElement) {
+    const displayCoffeeNotesContainer = `
+    <div id='displayCoffeeNotesPageContainer'>
+        <div id='filterContainer'>
+            <button type='button'>Filter Notes</button>
+        </div>
+        <div id='notesContainer'></div>
+    </div>`
+    container.innerHTML = '';
+    container.insertAdjacentHTML('afterbegin', displayCoffeeNotesContainer);
+
+    
+
+    const coffeeNotes = [];
+    const querySnapshot = await getDocs(collection(db, 'users', user.uid, 'coffee-notes'));
+    querySnapshot.forEach((doc) => {
+        coffeeNotes.push(doc.data());
+    })
+
+    console.log(coffeeNotes);
+
+    const notesContainer = document.getElementById('notesContainer') as HTMLDivElement;
+    coffeeNotes.forEach((note) => {
+        const HTMLNote = `
+            <div id='${note.id}' class='note-containers'>
+                <h3>${note.coffee}</h3>
+                <p>Date Tried: ${note.dateTried}</p>
+                <p>Rating: ${note.rating}</p>
+                <p>Notes:</p>
+                <p>${note.notes}</p>
+            </div>
+        
+        `
+        notesContainer.insertAdjacentHTML('beforeend', HTMLNote)
+    })
+
 
 }
